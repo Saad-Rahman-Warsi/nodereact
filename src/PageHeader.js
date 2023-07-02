c4e634b73d6a6241040b583bb8b288d66bc8c209
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-02 06:09:03 -0400

    dep

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -18,18 +18,18 @@ function PageHeader() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/AboutUs">About Us</Nav.Link>
-            <Nav.Link href="/Facilities">Facilities</Nav.Link>
-            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link to="/AboutUs">About Us</Nav.Link>
+            <Nav.Link to="/Facilities">Facilities</Nav.Link>
+            <Nav.Link to="#link">Services</Nav.Link>
 
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item to="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item to="#action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
